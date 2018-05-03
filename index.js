4665dc9bcf9d8b2fc2305c4349d3a16eab79e87b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-03 22:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {return driver.revenue > revenue})
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const names = drivers.filter(function (driver) {return driver.revenue > revenue})
+  return names.map(function (name) {return name})
+}
