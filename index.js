9562206b9a26ce1533fe84057668f819c2fede82
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-03 22:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 }
 
 function exactMatch(drivers, object) {
-  let keys = Object.keys(object)
-  let values = Object.values(object)
+  const keys = Object.keys(object)
+  const values = Object.values(object)
   return drivers.filter(function (driver) {return driver[keys[0]] === values[0]})
+}
+
+function exactMatchToList(drivers, object) {
+  const keys = Object.keys(object)
+  const values = Object.values(object)
+  const names = drivers.filter(function (driver) {return driver[keys[0]] === values[0]})
+  return names.map(function (name) {return name.name})
 }
